4ae20d81c0925d8d8496afcae3914b44e1e2e3f1
fusionconfusion <nendomatt@gmail.com>
2026-06-19 15:23:32 +0200

    Add files via upload
    PWA icon
    + manifest

---
+++ sw.js
@@ -0,0 +1,20 @@
+// Service worker minimal pour rendre le site installable en PWA
+const CACHE_NAME = 'fusion-confusion-v1';
+const urlsToCache = ['./index.html', './manifest.json', './icon-192.png', './icon-512.png'];
+
+self.addEventListener('install', event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME).then(cache => cache.addAll(urlsToCache))
+  );
+  self.skipWaiting();
+});
+
+self.addEventListener('activate', event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener('fetch', event => {
+  event.respondWith(
+    caches.match(event.request).then(response => response || fetch(event.request))
+  );
+});
